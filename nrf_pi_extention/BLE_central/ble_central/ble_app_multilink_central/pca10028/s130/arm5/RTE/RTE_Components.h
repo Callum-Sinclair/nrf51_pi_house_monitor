6fc4bbf57861667459292ac80c167785251b524a
Callum-Sinclair <1101952s@student.gla.ac.uk>
2016-04-10 14:56:06 +0100

    adding pi side nrf code project, currently just template

---
+++ nrf_pi_extention/BLE_central/ble_central/ble_app_multilink_central/pca10028/s130/arm5/RTE/RTE_Components.h
@@ -0,0 +1,26 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_multilink_central_s130_pca10028' 
+ * Target:  'nrf51422_xxac_s130' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+#define BSP_DEFINES_ONLY
+#define NRF_LOG_USES_UART=1
+#define S130
+#define SOFTDEVICE_PRESENT
+#define SWI_DISABLE0
+#define UART0_ENABLED
+
+#endif /* RTE_COMPONENTS_H */
